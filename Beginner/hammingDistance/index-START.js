764dad9138decb5982fac7a62b138b1551b2241b
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-11-22 16:48:34 -0600

    hamming distances successfully hammed

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -3,11 +3,26 @@ Given two strings of equal length, calculate and return the the hamming distance
 E.g hammingDistance('rover', 'river') // should return 1
 */
 
+// input: 2 strings of equal length
+// return: the min num of changes needed to turn one string into the other
+
+// find the number of differences
+// count the differences
 
 
+// function hammingDistance(stringA, stringB) {
+//     let count = 0
+//     for(let i = 0; i <= stringA.length - 1; i ++) {
+//         if(stringA[i] !== stringB[i]) count += 1
+//     }
+//     return count
+// }
 
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    return stringA.split('').reduce((acc, curr, i, arr) => {
+        if(arr[i] !== stringB[i]) return acc += 1
+        else return acc
+    }, 0)
 }
 
 
