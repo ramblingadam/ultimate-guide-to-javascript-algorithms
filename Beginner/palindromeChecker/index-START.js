1e44c33e1548aea745a36f2dee6d459f3d56bbd3
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-11-22 16:30:25 -0600

    palindromchecker optimized

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -6,8 +6,40 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
+// function palindromeChecker(text) {
+//     const reversed = text.split('').reverse().join('')
+//     return text === reversed
+// }
+
+// function palindromeChecker(text) {
+//     let reversed = ''
+//     for(let i = text.length - 1; i >= 0; i --) {
+//         reversed += text[i]
+//     }
+//     return text === reversed
+// }
+
+
+
 function palindromeChecker(text) {
-    v// Code goes here
+    let result = true
+    for(let i = 0; i <= Math.floor(text.length) / 2; i ++) {
+        if(text[i] !== text[text.length - i - 1]) {
+            result = false
+            break
+        }
+    }
+    return result
+}
+
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
+
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index - 1];
+    })
+
+    return result
 }
 
 
