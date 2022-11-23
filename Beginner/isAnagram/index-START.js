512a66cb77259985547c9a96f374f5e2159df339
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-11-23 14:11:12 -0600

    anagram SOLVED

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -6,9 +6,46 @@ don't. E.g
 */
 
 
+// input: 2 strings
+// return true if anagram, false if not
+
+// can the letters from one word be rearranged to form the other word
+// base case testing length, if lengths don't match, exit
+
+
+// make a cache for each string, compare caches
 
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    if(stringA.length !== stringB.length) return false
+
+    const cacheA = {}
+    const cacheB = {}
+
+    for(let char of stringA) {
+        if(cacheA[char]) cacheA[char] += 1
+        else cacheA[char] = 1
+    }
+
+    for(let char of stringB) {
+        if(cacheB[char]) cacheB[char] += 1
+        else cacheB[char] = 1
+    }
+
+    for(let char in cacheA) {
+        if(cacheA[char] !== cacheB[char]) return false
+    }
+
+    return true
+
+}
+
+// sort both strings and compare
+
+function isAnagram(stringA, stringB) {
+    const sortedA = stringA.split('').sort().join('')
+    const sortedB = stringB.split('').sort().join('')
+    if(sortedA === sortedB) return true
+    else return false
 }
 
 
