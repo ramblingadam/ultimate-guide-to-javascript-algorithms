efea85180df83c1f47bd1e448efd12be7dea91b4
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-11-23 16:19:19 -0600

    falsy1

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -6,8 +6,25 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
 
-function falsyBouncer(array) {
-    // Code goes here
+function falsyBouncer1(arr) {
+    return arr.filter(val => val)
+}
+
+function falsyBouncer2(arr) {
+    const falsy = [NaN, 0, '', undefined, null]
+    return arr.filter(val => !falsy.includes(val))
+}
+
+function falsyBouncer3(arr) {
+    const falsy = [NaN, 0, '', undefined, null]
+    
+    const result = []
+
+    for(let i = 0; i < arr.length; i ++) {
+        if(arr[i]) result.push(arr[i])
+    }
+
+    return result
 }
 
 
